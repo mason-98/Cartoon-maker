0dad1e635e4969191570c78c5029b007ffb30182
Mason <masontang@hotmail.com>
2025-05-30 21:06:13 -0400

    Added more deployment fixes

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -6,7 +6,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import type { ToastProps } from "./toast.types"
 
 const ToastProvider = ToastPrimitives.Provider
 
